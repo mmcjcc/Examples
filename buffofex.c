2039de5f42925088be567020712b989b98d8cdd9
Jason Cohen <jcohen@mmcjcc.com>
2020-01-05 21:17:42 -0500

    Add files via upload

---
+++ buffofex.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+/*
+Example of Buffer overflow from https://www.thegeekstuff.com/2013/06/buffer-overflow/
+*/
+int main(void)
+{
+    char buff[15];
+    int pass = 0;
+
+    printf("\n Enter the password : \n");
+    gets(buff);
+
+    if(strcmp(buff, "thegeekstuff"))
+    {
+        printf ("\n Wrong Password \n");
+    }
+    else
+    {
+        printf ("\n Correct Password \n");
+        pass = 1;
+    }
+
+    if(pass)
+    {
+       /* Now Give root or admin rights to user*/
+        printf ("\n Root privileges given to the user \n");
+    }
+
+    return 0;
+}
